7211860855952d9dbdc5ddda69e3939e98323c69
黄中瑶 <17685306043@163.com>
2024-07-29 14:36:21 +0800

    feat:(代码生成) 定义代码生成页面

---
+++ src/views/generate/api.js
@@ -0,0 +1,5 @@
+/**
+ * @Description: 代码生成api
+ * @Author: zuuuYao
+ * @Copyright: © 2024 HuangZhongYao | https://github.com/HuangZhongYao
+ */
---
+++ src/views/generate/index.vue
@@ -0,0 +1,23 @@
+<!--
+ @Description: 代码生成api
+ @Author: zuuuYao
+ @Copyright: © 2024 HuangZhongYao | https://github.com/HuangZhongYao
+-->
+<template>
+  <CommonPage>
+    <template #action>
+      <NButton type="primary" size="small" @click="handleAdd()">
+        <i class="i-material-symbols:add mr-4 text-18" />
+        新增角色
+      </NButton>
+    </template>
+  </CommonPage>
+</template>
+
+<script setup lang="js">
+
+</script>
+
+<style scoped>
+
+</style>
